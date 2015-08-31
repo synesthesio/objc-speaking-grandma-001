8f42e5e77e110aa6b71ea560ae2f82d48af3e056
synesthesio <msinger681@gmail.com>
2015-08-31 19:35:01 -0400

    Commit Attempt #2

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -16,13 +16,48 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here!
-     
-     */
+    
+    
+    
+    
+    
+NSString *talkToGrandma = @"Hi, Grandma!";
+    
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    
+BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+if (shouting) {
+    NSLog(@"NO, NOT SINCE 1938!");
+}
+    
+else {
+    NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+}
+    
+    
+if ([talkToGrandma isEqualToString:shoutAtGrandma]) {
+    NSLog(@"NO, NOT SINCE 1938!");}
+else {NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!"); }
+
+    
+BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    if (!shouting) {
+        NSLog(@"WHAT'S THAT? SPEAK UP, DEAR!");
+    }
+    
+    else {
+        NSLog(@"NO, NOT SINCE 1938!");
+    }
+    
+NSString *talkToGrandma = @"HI, GRANDMA!";
+    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
+    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
+    
+    talkToGrandma = @"Hi, Grandma!";
+    shoutAtGrandma = [talkToGrandma uppercaseString];
     
     return YES; // Don't alter this statement.
 }
